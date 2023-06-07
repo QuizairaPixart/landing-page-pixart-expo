0c40d4ec16231b687151f0dadf9cbd6ee9c04176
QuizairaPixart <quizaira.rodriguez@pixartargentina.com.ar>
2023-06-07 19:25:45 -0300

    popup estático

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let btnGamer = document.querySelector("#btn-gamer");
+let btnEmpresarial = document.querySelector("#btn-empresarial");
+let btnBraille = document.querySelector("#btn-braille");
+let btnEducativa = document.querySelector("#btn-educativa");
+
+let popupBraille = document.querySelector("#popup_braille");
+
+btnGamer.addEventListener("click", function(e){
+     
+})
+// console.log(btnGamer);
