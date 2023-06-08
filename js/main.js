208747de3351ecae168e87c7f9d663b8cd680dfa
QuizairaPixart <quizaira.rodriguez@pixartargentina.com.ar>
2023-06-08 01:48:42 -0300

    carousel funcionalidad

--- js/main.js
+++ js/main.js
@@ -14,53 +14,80 @@ let btnBraille = document.querySelector("#btn-braille");
 let popupBraille = document.querySelector("#popup_braille");
 let btnCloseBraille = document.querySelector("#btn-popup-braille");
 
+let carouselNews = document.querySelector("#carousel_news");
 let slider = document.querySelector("#slider");
 let sliderElement = document.querySelectorAll(".slider_element");
 let sliderPrev = document.querySelector("#prev");
 let sliderNext = document.querySelector("#next");
 
 btnBraille.addEventListener("click", function (e) {
+    e.preventDefault();
     popupBraille.classList.add("open-popup");
 });
 
 btnCloseBraille.addEventListener("click", function (e) {
+    e.preventDefault();
     popupBraille.classList.remove("open-popup");
 });
 
 btnEducativa.addEventListener("click", function (e) {
+    e.preventDefault();
     popupEducativa.classList.add("open-popup");
 });
   
 btnCloseEducativa.addEventListener("click", function (e) {
+    e.preventDefault();
     popupEducativa.classList.remove("open-popup");
 });
 
 btnEmpresarial.addEventListener("click", function (e) {
+    e.preventDefault();
     popupEmpresarial.classList.add("open-popup");
 });
 
 btnCloseEmpresarial.addEventListener("click", function (e) {
+    e.preventDefault();
     popupEmpresarial.classList.remove("open-popup");
 });
 
 btnGamer.addEventListener("click", function (e) {
+    e.preventDefault();
     popupGamer.classList.add("open-popup");
 });
 
 btnCloseGamer.addEventListener("click", function (e) {
+    e.preventDefault();
     popupGamer.classList.remove("open-popup");
 });
 
-// let imageIndex = 1, intervalId;
+let imageIndex = 1;
+let intervalId;
 
-// const autoSlide = () => {
-//     intervalId = setInterval(()=> slideImage(++imageIndex), 5000);
-// };
+const autoSlide = () => {
+    intervalId = setInterval(()=> slideImage(++imageIndex), 3000);
+};
+
+autoSlide();
+
+const slideImage = () => {
+    imageIndex = imageIndex === sliderElement.length ? 0 : imageIndex < 0 ? sliderElement.length - 1 : imageIndex;
+    slider.style.transform = `translate(-${imageIndex*100}%)`;
+}
+
+const updateClick = (e) => {
+    console.log(e);
+    e.preventDefault();
+    clearInterval(intervalId);
+    imageIndex += e.target.id === "next" ? 1 : -1;
+    slideImage(imageIndex);
+    autoSlide();
+}
+
+carouselNews.addEventListener("mouseover", ()=> clearInterval(intervalId));
+
+carouselNews.addEventListener("mouseleave", autoSlide);
 
 
+sliderPrev.addEventListener("click", updateClick);
 
-// autoSlide();
-
-// const slideImage = () => {
-//     slider.style.transform = `translate(-${imageIndex*100}%)`;
-// }
+sliderNext.addEventListener("click", updateClick);
